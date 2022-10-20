0deaa5bea092efa953eb0d9e31dd6eb3af340a19
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-10-20 15:35:45 +0100

    Update azrevm.tf

--- azrevm.tf
+++ azrevm.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         azurerm = {
             source  = "hashicorp/azurerm"
-            version = ">=2.46.0"
+            version = "=3.19.1"
             }
         }
     required_version = ">= 0.14.1"
